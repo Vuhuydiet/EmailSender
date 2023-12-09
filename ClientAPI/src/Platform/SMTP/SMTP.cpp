7ff8e5c84cb827173ae205b65743f93ef7862d3f
hytaty <22120418@student.hcmus.edu.vn>
2023-12-09 18:31:43 +0700

    updated SendMail and SendFile
    completed send mail feature

--- ClientAPI/src/Platform/SMTP/SMTP.cpp
+++ ClientAPI/src/Platform/SMTP/SMTP.cpp
@@ -14,6 +14,70 @@ namespace SMTP {
 		}
 		return res;
 	}
+	std::vector<int> CreateDate() {
+		// Get the current time point
+		std::time_t currentTime = std::time(nullptr);
+
+		std::tm* localTime = std::localtime(&currentTime);
+	
+		// Extract date and time components
+		std::vector<int> date;
+		
+		date.push_back(localTime->tm_mday);
+		date.push_back(localTime->tm_mon + 1);
+		date.push_back(localTime->tm_year + 1900);
+		date.push_back(localTime->tm_hour);
+		date.push_back(localTime->tm_min);
+		date.push_back(localTime->tm_sec);
+
+		return date;
+	}
+
+	std::string CreateMessageId(std::string sender) {
+		std::vector<int> currentDate = CreateDate();
+		// Output the current time
+		std::string id ="<";
+		char delim = '-';
+		id += base64_encode("0" + std::to_string(currentDate[2]) + "th") + delim;
+		for (int i = 3; i < currentDate.size(); i++) {
+			id += base64_encode(std::to_string(currentDate[i])) + delim;
+		}
+		// get user name from address mail
+		size_t atPos = sender.find('@');
+		if (atPos != std::string::npos) {
+			id += base64_encode(sender.substr(0, atPos));
+		}
+		else { // get all address mail
+			id += base64_encode(sender);
+		}		
+		return id+"@gmail.com>";
+	}
+
+	std::string CreateBoundary(){
+		// Output the current time
+
+		std::vector<int> currentDate = CreateDate();
+		// Output the current time
+		std::string id;
+		for (int i = 3; i < currentDate.size(); i++) {
+			id += base64_encode(std::to_string(currentDate[i]));
+		}
+		return id;
+	}
+
+	std::string CreateDateMail() {
+		std::vector<int> DateMailInit = CreateDate();
+		std::string DateMail;
+		for (int i = 0; i < DateMailInit.size(); i++) {
+			if (i < 3) {
+				DateMail += std::to_string(DateMailInit[i]) + "/";
+				if (i == 2) DateMail += " ";
+			}
+			else DateMail += std::to_string(DateMailInit[i]) + ":";
+
+		}
+		return DateMail;
+	}
 
 	void SendMail(Ref<Socket> socket, const SentMail& mail) {
 		if (!socket->IsConnected()) {
@@ -47,16 +111,51 @@ namespace SMTP {
 		socket->Receive();
 
 		// Header
+		// Initialize
+		const std::string boundary = CreateBoundary();
+		std::string Msg_ID = CreateMessageId(mail.Sender);
+		std::string DateMail = CreateDateMail();
+
+		// Send Header
+		if (!mail.AttachedFilePaths.empty()) {
+			socket->Send("Content-Type: multipart/mixed;boundary=\"" + boundary + "\"");
+		}
+		socket->Send("Message ID: "+ Msg_ID);
+		socket->Send("DATE: " + DateMail);
+		socket->Send("MIME-Version: 1.0");
+		socket->Send("User - Agent: C++ Client");
+		socket->Send("Content-Langaue: en-US");
 		socket->Send(FMT::format("From: <{}>", mail.Sender));
 		socket->Send(FMT::format("To: {}", AddressListToString(mail.Tos)));
 		socket->Send(FMT::format("Cc: {}", AddressListToString(mail.Ccs)));
 		socket->Send(FMT::format("Subject: {}", mail.Subject));
 		
-		socket->Send("");
+
+		if (!mail.AttachedFilePaths.empty()) {
+			socket->Send("\n");
+			socket->Send("This is a multi-part message in MIME format.");
+			socket->Send("--" + boundary);
+		}
+		
+		// Header of content
+		socket->Send("Content-Type:text/plain;charest=UTF-8;format=flowed");
+		socket->Send("Content-Transfer-Encoding: 7bit");
+		socket->Send("\n");
 
 		// Content
 		for (const auto& line : mail.Content) {
 			socket->Send(line);
+		}
+		socket->Send("\n");
+
+		// Attach file
+		if (!mail.AttachedFilePaths.empty()) {
+			for (int i = 0; i < mail.AttachedFilePaths.size(); i++) {
+				socket->Send("--" + boundary);
+				SendFile(socket, mail.AttachedFilePaths[i]);
+				socket->Send("\n");
+			}
+			socket->Send("--" + boundary);
 		}
 
 		// End sending data
--- ClientAPI/src/Platform/SMTP/SMTP.h
+++ ClientAPI/src/Platform/SMTP/SMTP.h
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <filesystem>
 #include "Core/Socket.h"
 #include "Email/SentMail.h"
-
 namespace SMTP {
 
 	void SendMail(Ref<Socket> socket, const SentMail& mail);
-
+	void SendFile(Ref<Socket> socket, const std::filesystem::path& filePath);
+	std::string base64_encode(const std::string& input);
 }
